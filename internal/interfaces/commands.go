46a1f3dec5c2a90e3ce3922ea5f29c34a40064f1
Sergey Matveychuk <sem@semmy.ru>
2025-11-16 23:13:35 +0300

    Forgotten file.

---
+++ internal/interfaces/commands.go
@@ -0,0 +1,22 @@
+package interfaces
+
+type Cmd byte
+
+const (
+	NoneCmd Cmd = iota
+	// Commands
+	ShutdownRequest = 1
+	ShutdownNotify  = 2
+	AskForResend    = 3
+	Ping            = 4
+	Pong            = 5
+	// Flags
+	NoEncryption = 0x80
+	NoSignature  = 0x40
+	WithPadding  = 0x20
+)
+
+const FlagsMask Cmd = 0xf0
+const CmdMask Cmd = 0x0f
+
+const HEADER = 9 // 2 bytes size + 2 bytes sequence number + 1 byte flags + 4 bytes CRC32
